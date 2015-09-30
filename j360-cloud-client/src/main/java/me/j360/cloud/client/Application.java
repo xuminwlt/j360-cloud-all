e15373b668d23d7a26b17012d5860bb5c6478c66
xumin <xumin_wlt@163.com>
2015-09-30 16:45:09 +0800

    configserver和client介绍

--- j360-cloud-client/src/main/java/me/j360/cloud/client/Application.java
+++ j360-cloud-client/src/main/java/me/j360/cloud/client/Application.java
@@ -2,6 +2,7 @@ package me.j360.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class Application {
 
     @RequestMapping("/")
--- j360-cloud-configserver/src/main/java/me/j360/cloud/configserver/ConfigServerApplication.java
+++ j360-cloud-configserver/src/main/java/me/j360/cloud/configserver/ConfigServerApplication.java
@@ -3,8 +3,11 @@ package me.j360.cloud.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
